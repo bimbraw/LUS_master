30889b8dbec7bf57b6269f43d3afb9b243fa6d30
bimbraw <bimbrawkeshav@gmail.com>
2020-12-24 06:36:03 -0500

    Added the prediction and corrected mean square error conditions.

--- Journal_Files/Data_Processing_means.py
+++ Journal_Files/Data_Processing_means.py
@@ -1,24 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import addcopyfighandler
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.metrics import mean_squared_error
 from sklearn import svm
-import sklearn
-from sklearn.model_selection import cross_validate
 
 folder = 'angle_v_7'
 xlabel_u = 'U values'
 xlabel_v = 'V values'
 
+u_or_v = 'v'
+kernel = 'poly'
+
 dataset = pd.read_csv('Data_Xihan_11-5_components/' + folder + '.csv')
 print(dataset)
 print(dataset.shape)
-X = dataset.iloc[:, 2].values
+X = dataset.iloc[:, 1].values
 X = X.reshape(len(X), 1)
-y = dataset.iloc[:, 1].values
-#y = np.ravel(y)
+y = dataset.iloc[:, 2].values
 
 #test training split
 #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -28,10 +29,10 @@ y = dataset.iloc[:, 1].values
 dataset = dataset.iloc[:, 1:3]
 
 #https://www.geeksforgeeks.org/python-ways-to-remove-duplicates-from-list/
-X_compressed = []
-for i in X:
-    if i not in X_compressed:
-        X_compressed.append(i)
+y_compressed = []
+for i in y:
+    if i not in y_compressed:
+        y_compressed.append(i)
 
 #added mean condition thanks to
 #https://stackoverflow.com/questions/64879466
@@ -53,22 +54,19 @@ print(means.index)
 print('Printing the first element -')
 print(means.index[0])
 
-val_x = []
-for i in means.index:
-    val_x.append(i)
-
-print('Here is the val_x list -')
-print(val_x)
-
 val_y = []
-for i in means.angle:
+for i in means.index:
     val_y.append(i)
 
 print('Here is the val_y list -')
 print(val_y)
 
-#holdout_score = -1 * metrics.mean_squared_error(val_y, val_x)
-#print(holdout_score)
+val_x = []
+for i in means.angle:
+    val_x.append(i)
+
+print('Here is the val_x list -')
+print(val_x)
 
 X_train, X_test, y_train, y_test = train_test_split(val_x, val_y, test_size=0.2, random_state=0)
 X_train = np.array(X_train).reshape(-1, 1)
@@ -77,53 +75,81 @@ y_train = np.array(y_train).reshape(-1, 1)
 y_test = np.array(y_test).reshape(-1, 1)
 print('Training set - X')
 print(X_train.shape)
-print(type(X_train))
 print(X_train)
 print('Test set - X')
 print(X_test.shape)
-print(type(X_test))
 print(X_test)
 print('Training set - y')
 print(y_train.shape)
-print(type(y_train))
 print(y_train)
 print('Test set - y')
 print(y_test.shape)
-print(type(y_test))
 print(y_test)
 
-#print(X_train.reshape(1, -1))
-#print(y_train.reshape(1, -1))
-#print(X_test.reshape(1, -1))
-#print(y_test.reshape(1, -1))
 
 X_train = X_train.astype('double')
 y_train = y_train.astype('double')
 X_test = X_test.astype('double')
 y_test = y_test.astype('double')
 
+
 print('Started Model training')
-clf = svm.SVR(kernel='rbf').fit(X_train, y_train)
+clf = svm.SVR(kernel=kernel).fit(X_train, y_train)
+print(clf)
 print('Model trained')
 
-#The coefficient of determination (R2 score)
-print('Here is the R2 score -')
-print(clf.score(X_test, y_test))
+#make predictions
+pred = clf.predict(X_test)
 
-#print(sorted(sklearn.metrics.SCORERS.keys()))
-
-#Mean Squared Error
-print('Here is the MSE score -')
-#print(cross_validate(clf, X_test, y_test, cv=3, scoring='neg_mean_squared_error'))
-print(mean_squared_error(X_test, y_test))
+print('Here is the mean squared error -')
+print(mean_squared_error(pred, y_test))
 
 fig = plt.figure()
-plt.scatter(X, y, color='pink', label='data')
-plt.plot(X_compressed, means)
-plt.xlabel(xlabel_v)
-plt.ylabel('Angle')
-plt.xlim(0, 255)
-plt.ylim(-50, 50)
+#print(X.reshape(1, -1))
+#print(y)
+plt.scatter(X.reshape(1, -1), y, color='yellow', label='original data')
+plt.scatter(val_x, val_y, color='pink', label='mean data')
+plt.plot(means, y_compressed, color='red', label='connected mean data')
+if u_or_v == 'u':
+    plt.ylabel(xlabel_u)
+if u_or_v == 'v':
+    plt.ylabel(xlabel_v)
+plt.xlabel('Angle')
+#plt.ylim(0, 255)
+#plt.xlim(-50, 50)
+plt.legend()
 plt.title('Output plot for ' + folder)
-#plt.savefig('C:/Users/bimbr/OneDrive/Desktop/Research/Lung_Ultrasound/Updated_plots/' + folder + '.png')
+plt.show()
+
+fig = plt.figure()
+#print(X_test.shape)
+#print(y_test)
+pred = np.reshape(pred, (-1, 1))
+#print(pred.shape)
+#print(pred)
+
+test_data = np.concatenate((np.array(X_test), np.array(y_test)), axis=1)
+test_data = pd.DataFrame(test_data)
+sorted_test = test_data.sort_values(by=test_data.columns[0])
+#print('Here is the sorted test data -')
+#print(sorted_test)
+pred_data = np.concatenate((np.array(X_test), np.array(pred)), axis=1)
+pred_data = pd.DataFrame(pred_data)
+sorted_pred = pred_data.sort_values(by=pred_data.columns[0])
+#print('Here is the sorted pred data -')
+#print(sorted_pred)
+#print(test_data[:, 1])
+#print(test_data)
+#print(sorted_pred[0])
+plt.scatter(sorted_test[0], sorted_test[1], color='black', label='test data')
+plt.plot(sorted_pred[0], sorted_pred[1], color='blue', label='prediction curve')
+if u_or_v == 'u':
+    plt.ylabel(xlabel_u)
+if u_or_v == 'v':
+    plt.ylabel(xlabel_v)
+plt.xlabel('Angle')
+#plt.ylim(0, 255)
+#plt.xlim(-50, 50)
+plt.legend()
+plt.title('Test data/prediction data ' + folder)
 plt.show()
